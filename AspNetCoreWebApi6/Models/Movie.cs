943bcd14e798a73cd5f2713ba7c4f3320b2b5d46
AdrianGaray <adriangaray396@gmail.com>
2022-07-17 17:29:48 -0300

    Add project files.

---
+++ AspNetCoreWebApi6/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreWebApi6.Models
+{
+    public class Movie
+    {
+        // The Id field is required by the database for the primary key.
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
